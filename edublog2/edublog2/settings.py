44dd56aca5a01931460ff0164fe33f4b37e9173c
anushanagula <nagulaanusha21@gmail.com>
2021-05-09 17:40:22 +0530

    requirements file

--- edublog2/edublog2/settings.py
+++ edublog2/edublog2/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
